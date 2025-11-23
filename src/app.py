21052a0a9a623c8aae50e956f3cdb45429dedecc
Japmjr <chuchitoperdomo@gmail.com>
2025-11-23 21:32:08 +0000

    terminado

--- src/app.py
+++ src/app.py
@@ -22,6 +22,6 @@ def delete_todo(position):
     
     return jsonify(todos)
 
-# These two lines should always be at the end of your app.py file
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
